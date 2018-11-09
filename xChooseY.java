bce4f2d7ef999a7fd2d31d3dec3ff196cbba859e
FallingLeaf79 <44774181+FallingLeaf79@users.noreply.github.com>
2018-11-09 22:46:48 +0100

    Updated xChooseY.java
    Redid the algorithm. Works flawlessly for values of x up to 34. Above 35 works only for values of y *not* close to 50% of x.

--- xChooseY.java
+++ xChooseY.java
@@ -1,14 +1,27 @@
 public class xChooseY {
-  public static long factorial(long x) {
+  /*public static long factorial(long x) {
     long out = 1L;
     while (x > 0) {
       out = out*x;
       x--;
     }
     return out;
-  }
+  }*/
 
   public static void main (String[] args) {
+    int x = Integer.parseInt(args[0]);
+    int y = Integer.parseInt(args[1]);
+    if (y > (x-y)) {
+      y = x-y;
+    } //(n*(n-1)*...(n-k+1))/(k*(k-1)*...*1)
+    int result = 1;
+    for (int z = 0; z < y; z++) {
+      result *= (x-z);
+      result /= (y-z);
+    }
+    System.out.println(result);
+  }
+  /*public static void main (String[] args) {
     long x = Long.parseLong(args[0]);
     long y = Long.parseLong(args[1]);
     if (x >= y && x >= 0 && y >= 0) {
@@ -18,5 +31,5 @@ public class xChooseY {
     else {
       System.out.println("0");
     }
-  }
+  }*/
 }
