4101b4239fdbe2ea355474a637ff27c1a399e2ce
FallingLeaf79 <44774181+FallingLeaf79@users.noreply.github.com>
2019-03-14 15:59:33 +0100

    Added Email.java
    Loads an "e-mail" from a text file and prints the sender and the subject.

---
+++ 3-2019/Email.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Email {
+
+	public static void main(String[] args) throws java.io.IOException {
+		Scanner email = new java.util.Scanner(java.nio.file.Paths.get("email.txt"));
+		
+		while (email.hasNextLine()) {
+			String nextThing = email.nextLine();
+			
+			if (nextThing.startsWith("From:") || nextThing.startsWith("Subject:")) {
+				System.out.println(nextThing);
+			}
+		}
+	}
+}
